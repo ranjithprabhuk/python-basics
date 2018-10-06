ddc136decfd8336d9d3d2786eeb6046b9bd4a2bd
Ranjithprabhu Kumar <ranjithprabhu.k@gmail.com>
2018-10-06 14:57:31 +0530

    Print a line in python

---
+++ 1_basic_syntax.py
@@ -0,0 +1,14 @@
+# Print a line python
+print('Hello Developer!!')
+print('Welcome to Python!!')
+
+# Break and print a line in python
+print('Hello Developer!!\nWelcome to Python!!')
+
+
+
+# Simple IF ELSE Block
+if True:
+    print('\nHey You are Python Developer Now!!\n')
+else:
+    print('Still needs to learn an lot buddy!!')
