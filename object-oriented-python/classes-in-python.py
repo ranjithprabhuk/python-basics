b273f224ed033d30837c32ebdfee50ee54181ebe
Ranjithprabhu <ranjithprabhu.k@gmail.com>
2019-08-18 21:37:07 +0530

    created a simple mobile class in python

---
+++ object-oriented-python/classes-in-python.py
@@ -0,0 +1,12 @@
+class Mobile:
+    def __init__(self, brand, price):
+        self.brand = brand
+        self.price = price
+
+mob1=Mobile("Apple", 20000)
+mob2=Mobile("Samsung", 10000)
+
+print(mob1.brand)
+print(mob1.price)
+print(mob2.brand)
+print(mob2.price)
