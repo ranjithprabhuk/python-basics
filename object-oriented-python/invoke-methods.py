0ff1a9d90729c9d1bd5fc6d449477284799cf562
Ranjithprabhu <ranjithprabhu.k@gmail.com>
2019-08-18 22:01:05 +0530

    simple example for method invokation in python

--- object-oriented-python/invoke-methods.py
+++ object-oriented-python/invoke-methods.py
@@ -1,9 +1,16 @@
 class Mobile:
+    def __init__(self,price,brand):
+        print (id(self))
+        self.price = price
+        self.brand = brand
+
     def display(self):
-        print("Displaying details")
+        print("Displaying details: \n price:" + str(self.price) + ", brand:" + str(self.brand))
 
     def purchase(self):
         self.display()
-        print("Calculating price")
+        print("Mobile purchased for Rs." + str(self.price) + ", brand:" + self.brand)
 
-Mobile().purchase()
+
+mobile = Mobile(20000, "Apple")
+mobile.purchase()
