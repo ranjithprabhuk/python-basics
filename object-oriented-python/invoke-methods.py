7b7f3546f6913a311ae7eea8b0eb85321a7c5731
Ranjithprabhu <ranjithprabhu.k@gmail.com>
2019-08-18 21:52:38 +0530

    invoke methods inside the class in python

---
+++ object-oriented-python/invoke-methods.py
@@ -0,0 +1,9 @@
+class Mobile:
+    def display(self):
+        print("Displaying details")
+
+    def purchase(self):
+        self.display()
+        print("Calculating price")
+
+Mobile().purchase()
