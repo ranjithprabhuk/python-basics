e338250ec0a9bf57f076808bf82a88e53da32180
Ranjithprabhu <ranjithprabhu.k@gmail.com>
2019-08-18 21:51:38 +0530

    print the id of the object inside the constructor

--- object-oriented-python/classes-in-python.py
+++ object-oriented-python/classes-in-python.py
@@ -2,6 +2,7 @@ class Mobile:
     def __init__(self, brand, price):
         self.brand = brand
         self.price = price
+        print("Id of self in constructor", id(self))
 
 mob1=Mobile("Apple", 20000)
 mob2=Mobile("Samsung", 10000)
